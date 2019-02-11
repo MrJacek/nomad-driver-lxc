07a90de4a0ff21f0c86287e033e898c09cccb37a
Jacek Hojczak <jacek.hojczak@euro.com.pl>
2019-02-11 21:32:40 +0100

    Compileing lxd plugin :D

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	log "github.com/hashicorp/go-hclog"
 
-	"github.com/nicecode/nomad-driver-lxc/lxc"
 	"github.com/hashicorp/nomad/plugins"
+	"github.com/nicecode/nomad-driver-lxd/lxd"
 )
 
 func main() {
@@ -14,5 +14,5 @@ func main() {
 
 // factory returns a new instance of the LXC driver plugin
 func factory(log log.Logger) interface{} {
-	return lxc.NewLXCDriver(log)
+	return lxd.NewLXDDriver(log)
 }
